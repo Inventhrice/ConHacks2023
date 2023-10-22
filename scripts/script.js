fbe592b2a481f93f6abfcb1dc7831bbf3b7ffec4
Inventhrice <avaneesha03@gmail.com>
2023-10-22 15:00:19 -0400

    fixed a mixup in names

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 
-function loadTemplates(templateName){
+function setTemplate(){
     let template = {"templateName":"Steve Hendriske", "fileExtensions":[".pdf"], "!fileExtensions":[".zip"]};
     window.localStorage.setItem(template.templateName, JSON.stringify(template));
     
 }
 
-function getTemplate(){
-    let get = JSON.parse(localStorage.getItem(test.templateName));
+function getTemplate(templateName){
+    let get = JSON.parse(localStorage.getItem(templateName));
 }
 
 function criteria(){
